af8fbde38f341c057ed16a165578bd9ea5acb7c1
hsg77 <hsg77@163.com>
2019-05-07 15:04:33 +0800

    添加 testInsertionPointCopy功能

--- src/main/java/com/cwgis/App.java
+++ src/main/java/com/cwgis/App.java
@@ -1,5 +1,10 @@
 package com.cwgis;
 
+import com.google.common.collect.Sets;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
 /**
  * Hello world!
  *
@@ -9,7 +14,31 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        test_sqlite_jdbc.main();
-        test_threads.main(new String[]{});
+        testInsertPointCopy();
+        //test_sqlite_jdbc.main();
+        //test_threads.main(new String[]{});
+    }
+    public static void testInsertPointCopy()
+    {
+        Byte[] testSide=new Byte[]{30,31};
+        int testLastIndex = testSide.length;
+        Byte tierLeft=15;
+        int firstGreater=Arrays.binarySearch(testSide,tierLeft);
+        System.out.println("testLastIndex="+testLastIndex);
+        System.out.println("firstGreater="+firstGreater);
+        int insertionPoint = Math.abs(firstGreater);
+        if (insertionPoint >= testLastIndex) {
+            // Not present in array, and none greater than this value
+            System.out.println("InsertPoint>=lastIndex");
+        }
+        HashSet<Byte> higherTiers = Sets.newHashSet(Arrays.copyOfRange(
+                testSide,
+                insertionPoint,
+                testLastIndex));
+        System.out.print("result=");
+        for(Byte t :higherTiers)
+        {
+            System.out.print(t+" ");
+        }
     }
 }
