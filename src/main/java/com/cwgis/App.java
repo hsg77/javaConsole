98ed41f437c5b4bc7e70a327c6ab33c6efc826de
hsg77 <hsg77@163.com>
2019-05-24 20:31:00 +0800

    测试scale

--- src/main/java/com/cwgis/App.java
+++ src/main/java/com/cwgis/App.java
@@ -18,7 +18,19 @@ public class App
         //test_sqlite_jdbc.main();     //OK
         //test_threads.main(new String[]{});   //OK
         //test_proj4j_coorTrans.main();   //OK
-        test_footPoint.main();
+        //test_footPoint.main();
+        testScale();
+    }
+    public static void testScale()
+    {
+        double earch_radius = 6371008.8D;   // 地球半径 平均值  米
+        double resolution=2*Math.PI*earch_radius;    //赤道分辨率    （像素/米）
+        System.out.println(resolution);
+        System.out.println(resolution/256);
+        double lat=30.77751;
+        double cpResolution=resolution*Math.cos(lat*Math.PI/180.0);   //某一纬度分辨率
+        double scale=cpResolution*72*32.3701;
+        System.out.println(scale);
     }
 
 
